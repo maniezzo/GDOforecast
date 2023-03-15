a074c6e3e39e9d7cd375c389b9f42d42ca8c23f9
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2023-03-15 17:24:28 +0100

    sarimax2

--- run_sarimax.py
+++ run_sarimax.py
@@ -8,11 +8,12 @@ def sarima(ds,indices,autoArima=False):
       model = pm.auto_arima(ds.values, exogenous=externals, start_p=1, start_q=1,
                             test='adf', max_p=3, max_q=3, m=12,
                             start_P=0, seasonal=True,
-                            d=None, D=1, trace=True,
+                            d=None, D=1,
+                            trace=False,
                             error_action='ignore',
                             suppress_warnings=True,
                             maxiter = 500,
-                            stepwise=True)  # False full grid
+                            stepwise=True)
       morder     = model.order
       mseasorder = model.seasonal_order
    else:
@@ -22,9 +23,9 @@ def sarima(ds,indices,autoArima=False):
 
    model = pm.arima.ARIMA(morder, seasonal_order=mseasorder, exogenous=externals, return_conf_int=True)
    fitted = model.fit(ds)
-   print(model.summary())
-   yfore,confint = fitted.predict(n_periods=3,exog=externals[9:12], return_conf_int=True)  # forecast
+   #print(model.summary())
    ypred = fitted.predict_in_sample(exog=externals)
+   yfore,confint = fitted.predict(n_periods=3,exog=externals[9:12], return_conf_int=True)  # forecast
    plt.plot(ds.values)
    plt.plot([x for x in ypred[1:]])
    plt.plot([None for i in ypred] + [x for x in yfore])
