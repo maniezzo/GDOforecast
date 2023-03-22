728563f42fdbe70ddae11467b1ed7bfb302820f3
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2023-03-22 18:15:39 +0100

    mar 22

---
+++ Lagrangian.py
@@ -0,0 +1,7 @@
+import numpy as np, pandas as pd
+import pulp
+
+if __name__ == "__main__":
+   print(3)
+
+   pass
--- data_exploration.py
+++ data_exploration.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
    for header in df.columns:
       if('error' in header):
          print(f"algo {header} mean = {np.mean(df[header])} median = {np.median(df[header])} stdev = {np.std(df[header])}")
+
    pass
