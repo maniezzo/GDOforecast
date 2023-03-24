7c6fbf395ebe6ae4c0dabe80be0b7e4751fa929c
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2023-03-24 15:18:44 +0100

    IP model

--- Lagrangian.py
+++ Lagrangian.py
@@ -1,7 +1,7 @@
 import numpy as np, pandas as pd
 import pulp
 
-def makeModel(requests, costs, cap):
+def makeModel(requests, costs, cap, b):
    ncol = 2*ncli*nser
    nx   = ncli*nser
    categ ='Binary'  # 'Continuous'
@@ -10,11 +10,11 @@ def makeModel(requests, costs, cap):
                         lowBound=0,
                         upBound=1)
 
-   Y = pulp.LpVariable.dicts('Y%s', (range(nx,ncol)),
+   Q = pulp.LpVariable.dicts('Y%s', (range(nx,ncol)),
                         cat='Integer',
                         lowBound=0,
                         upBound=50)
-   X.update(Y) # append Y to X
+   X.update(Q) # append Q to X
 
    # create the LP object, set up as a MINIMIZATION problem
    probl = pulp.LpProblem('GDO', pulp.LpMinimize)
@@ -23,21 +23,27 @@ def makeModel(requests, costs, cap):
    c[0:nx] = [costs[i,j] for i in np.arange(nser) for j in np.arange(ncli)]
    probl += sum(c[i] * X[i] for i in range(nx))
 
-   # knapsack constraints
+   # knapsack constraints Sum qij leq Qi
    nrows = 0
    for i in np.arange(nser):
-      probl += sum(requests[i,j]*X[i*ncli+j] for j in np.arange(0,ncli)) <= cap[i], "cap%d" % nrows
+      probl += sum(X[nx+i*ncli+j] for j in np.arange(0,ncli)) <= cap[i], "cap%d" % nrows
       nrows += 1
 
-   # assignment constraints
-   for i in np.arange(ncli):
-      probl += sum(X[j*ncli+i] for j in np.arange(0,nser)) == 1, f"ass{nrows}"
+   # assignment constraints Sum qij = reqj
+   for j in np.arange(ncli):
+      probl += sum(X[nx+i*ncli+j] for i in np.arange(0,nser)) == requests[j], f"ass{nrows}"
       nrows += 1
 
-   # amount constraint
-   for i in np.arange(nx):
-      probl += sum(X[nx+j*ncli+i] for j in np.arange(0,nser)) >= 1, f"amt{nrows}"
+   # amount constraint Sum xij leq bj
+   for j in np.arange(ncli):
+      probl += sum(X[i*ncli+j] for i in np.arange(0,nser)) <= b[j], f"b{nrows}"
       nrows += 1
+
+   # x - q
+   for i in np.arange(nser):
+      for j in np.arange(ncli):
+         probl += X[nx+i*ncli+j] - requests[j]*X[i*ncli+j] <= 0, f"xq{nrows}"
+         nrows += 1
 
    # save the model in a lp file
    probl.writeLP("GDOmodel.lp")
@@ -62,8 +68,11 @@ if __name__ == "__main__":
    dfreq  =pd.read_csv("requests.csv")
    ncli = 52
    nser = 4
-   (cost,sol) =  makeModel(dfreq.iloc[0:nser,0:ncli].values,
+   b = np.ones(ncli)
+   b[1] = 1
+   (cost,sol) =  makeModel(dfreq.iloc[0,0:ncli].values,
                            dfcosts.iloc[0:nser,0:ncli].values,
-                           dfreq.iloc[0:nser,ncli].values)
+                           dfreq.iloc[0:nser,ncli].values,
+                           b)
 
    pass
