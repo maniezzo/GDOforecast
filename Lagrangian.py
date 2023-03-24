dc8933b2bca9761ff086f0f19c834a5662a72b17
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2023-03-24 17:06:49 +0100

    mar 23

--- Lagrangian.py
+++ Lagrangian.py
@@ -60,7 +60,76 @@ def makeModel(requests, costs, cap, b):
       v = probl.variables()[i]
       if (v.varValue > 0):
          sol.append({'i': i%ncli, 'ser': i//ncli})
-         print(f"{v} = {v.varValue}  i: {i%ncli}, ser: {i//ncli}")
+         #print(f"{v} = {v.varValue}  i: {i%ncli}, ser: {i//ncli}")
+   return (cost,sol)
+
+def subProblem(requests, costs, cap, b, vlambda):
+   ncol = 2*ncli*nser
+   nx   = ncli*nser
+   categ ='Binary'  # 'Continuous'
+   X = pulp.LpVariable.dicts('X%s', (range(nx)),
+                        cat=categ,
+                        lowBound=0,
+                        upBound=1)
+
+   Q = pulp.LpVariable.dicts('Y%s', (range(nx,ncol)),
+                        cat='Integer',
+                        lowBound=0,
+                        upBound=50)
+   X.update(Q) # append Q to X
+
+   # create the LP object, set up as a MINIMIZATION problem
+   probl = pulp.LpProblem('GDO', pulp.LpMinimize)
+   # cost function
+   c = np.zeros(ncol)
+   c[0:nx] = [costs[i,j] for i in np.arange(nser) for j in np.arange(ncli)]
+   probl += sum(c[i] * X[i] for i in range(nx))
+
+   nrows = 0
+   # assignment constraints Sum qij = reqj
+   for j in np.arange(ncli):
+      probl += sum(X[nx+i*ncli+j] for i in np.arange(0,nser)) == requests[j], f"ass{nrows}"
+      nrows += 1
+
+   # amount constraint Sum xij leq bj
+   for j in np.arange(ncli):
+      probl += sum(X[i*ncli+j] for i in np.arange(0,nser)) <= b[j], f"b{nrows}"
+      nrows += 1
+
+   # x - q
+   for i in np.arange(nser):
+      for j in np.arange(ncli):
+         probl += X[nx+i*ncli+j] - requests[j]*X[i*ncli+j] <= 0, f"xq{nrows}"
+         nrows += 1
+
+   # save the model in a lp file
+   probl.writeLP("GDOmodel.lp")
+   # view the model
+   print(probl)
+
+   # solve the model
+   probl.solve()
+   print("Status:", pulp.LpStatus[probl.status])
+   cost = pulp.value(probl.objective)
+   print("Objective: ", cost)
+   sol = []
+   for i in np.arange(ncol):
+      v = probl.variables()[i]
+      if (v.varValue > 0):
+         sol.append({'i': i%ncli, 'ser': i//ncli})
+         #print(f"{v} = {v.varValue}  i: {i%ncli}, ser: {i//ncli}")
+   return (cost,sol)
+
+def checkFeas(sol):
+   return True
+def subgradient(requests,costs,cap,b):
+   vlambda = np.zeros(nser)
+   cont = 0
+   while(cont < 3):
+      (cost,sol) = subProblem(requests,costs,cap,b,vlambda)
+      checkFeas(sol)
+      cont += 1
+
    return (cost,sol)
 
 if __name__ == "__main__":
@@ -74,5 +143,11 @@ if __name__ == "__main__":
                            dfcosts.iloc[0:nser,0:ncli].values,
                            dfreq.iloc[0:nser,ncli].values,
                            b)
+   print(f"IP model, cost {cost}")
 
+   (cost,sol) =  subgradient(dfreq.iloc[0,0:ncli].values,
+                           dfcosts.iloc[0:nser,0:ncli].values,
+                           dfreq.iloc[0:nser,ncli].values,
+                           b)
+   print(f"lagrangian model, cost {cost}")
    pass
