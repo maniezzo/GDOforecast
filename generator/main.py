b905a2329fc1a68755db98dd8bcec9604c97d79e
maniezzo <vittorio.maniezzo@unibo.it>
2024-09-26 12:34:04 +0200

    sept26

--- generator/main.py
+++ generator/main.py
@@ -2,16 +2,21 @@ import numpy as np
 import pandas as pd
 
 if __name__ == "__main__":
+   # row: server, col: client
    df = pd.read_csv('seedMatrix.csv',header=0)
-   df = df.iloc[:, 1:]
-   # Iterate over upper triangle of the matrix
-   for i in range(df.shape[0]):
-      for j in range(i+1,df.shape[1]):
-         if (df.iloc[i,j]==999999 and i!=j): df.iloc[i,j] = df.iloc[j,i]
-         if abs(df.iloc[i,j] - df.iloc[j,i]) > 150:
-            print(f"i {i} j {j} {df.iloc[j, i]} => {df.iloc[i, j]}")
-            df.iloc[j,i]=df.iloc[i,j]
-   df.to_csv('seedMatrix.csv')
-   dforg = df.iloc[0:4, 305:]
-   dforg.to_csv("cost1.csv")
+   dforg = df.iloc[0:52, 0:4].T
+   arr = dforg.to_numpy()
+   dforg.to_csv("cost1.csv", header=False, index=False)
+
+   # Randomly select a subset of rows and columns
+   num_rows    = 4  # Number of rows to sample
+   num_columns = 2  # Number of columns to sample
+
+   # Randomly select rows
+   instance_rows = df.sample(n=num_rows, random_state=42)  # Set random_state for reproducibility
+   # Randomly select columns
+   instance_columns = instance_rows.sample(n=num_columns, axis=1, random_state=42)
+
+   cost = instance_columns.T
+
    print("Fine")
