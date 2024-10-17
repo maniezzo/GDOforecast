faac0db7355456ab424195ce0cddcc2758a8a78b
maniezzo <vittorio.maniezzo@unibo.it>
2024-10-17 15:32:04 +0200

    oct17

---
+++ stochastic/deterministic.h
@@ -0,0 +1,27 @@
+#ifndef STOCHASTIC_H
+#define STOCHASTIC_H
+
+#include "common.h"
+
+class SingleMIP
+{
+public:
+   string name;
+
+   int n,m;    // num clients e servers
+   int nmult, maxReq;
+
+   vector<int> b;       // number of allowed assignments
+   vector<int> cap;     // DC capacities
+   vector<int> req;     // client requests
+   vector<int> cols;    // cost matrix columns
+   vector<int> rows;    // cost matrix rows
+   vector<int> qcost;   // DC rental cost
+
+   vector<vector<int>> xAssCost; // assignment costs
+
+   int populateTableau(CPXENVptr env, CPXLPptr lp);
+   tuple<int,int,int,float,float,double,double> solveMIP(int timeLimit, bool isVerbose);
+   void readInstance(string& fileName);
+};
+#endif // STOCHASTIC_H
